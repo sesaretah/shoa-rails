54387ade5ac3ba49d4d6de0ba20e91feeff3d915
sesaretah <hs.shafiei@gmail.com>
2020-08-24 15:55:43 +0430

    1

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,11 +1,23 @@
 class CommentSerializer < ActiveModel::Serializer
   include ActionView::Helpers::TextHelper
-  attributes :id, :content, :created_at, :profile, :reply_to
+  attributes :id, :content, :created_at, :profile, :reply_to, :editable
   def profile 
      ProfileSerializer.new(object.profile).as_json
   end 
 
   def reply_to
     truncate(object.reply.content) if object.reply
+  end
+
+  def editable
+    flag = false
+    if scope && scope[:user_id]
+      user = User.find(scope[:user_id])
+      flag =  true if object.user_id == user.id
+      role = object.commentable.user_role(user) if user
+      access = object.commentable.access(role) if role
+      flag =  true if access && access.include?('edit') 
+      return flag
+    end
   end
 end
