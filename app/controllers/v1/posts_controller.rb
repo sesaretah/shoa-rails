ceb5469f8507777e173f2c9b3181b64c0db70c7a
sesaretah <hs.shafiei@gmail.com>
2020-08-24 16:12:03 +0430

    1

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -4,7 +4,7 @@ class V1::PostsController < ApplicationController
     #@friend_ids = Friendship.friend_ids(current_user)
     #posts = Post.where('user_id IN (?)', @friend_ids).order('created_at desc')
     posts = Post.all.order('updated_at DESC').paginate(page: params[:page], per_page: 6)
-    render json: { data: ActiveModel::SerializableResource.new(posts, user_id: current_user.id,  each_serializer: PostSerializer ).as_json, klass: 'Post' }, status: :ok
+    render json: { data: ActiveModel::SerializableResource.new(posts, user_id: current_user.id,  each_serializer: PostSerializer, scope: {user_id: current_user.id} ).as_json, klass: 'Post' }, status: :ok
   end
 
   def search
@@ -13,7 +13,7 @@ class V1::PostsController < ApplicationController
     else 
       posts = Post.paginate(page: params[:page], per_page: 6)
     end
-    render json: { data: ActiveModel::SerializableResource.new(posts,  each_serializer: PostSerializer ).as_json, klass: 'Post' }, status: :ok
+    render json: { data: ActiveModel::SerializableResource.new(posts,  each_serializer: PostSerializer, scope: {user_id: current_user.id} ).as_json, klass: 'Post' }, status: :ok
   end
 
 
@@ -28,14 +28,14 @@ class V1::PostsController < ApplicationController
     @post.user_id = current_user.id
     if @post.save
       @post.share(params[:channel_id]) if !params[:channel_id].blank?
-      render json: { data: PostSerializer.new(@post).as_json, klass: 'Post' }, status: :ok
+      render json: { data: PostSerializer.new(@post, scope: {user_id: current_user.id}).as_json, klass: 'Post' }, status: :ok
     end
   end
 
   def update
     @post = Post.find(params[:id])
     if @post.update_attributes(post_params)
-      render json: { data: PostSerializer.new(@post, user_id: current_user.id).as_json, klass: 'Post' }, status: :ok
+      render json: { data: PostSerializer.new(@post, scope: {user_id: current_user.id}, user_id: current_user.id).as_json, klass: 'Post' }, status: :ok
     else
       render json: { data: @post.errors.full_messages  }, status: :ok
     end
