8f7fd9bf35711409f1c6b4b56771d6d8633c7f2f
sesaretah <hs.shafiei@gmail.com>
2020-09-02 09:57:33 +0430

    1

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -14,6 +14,7 @@ class CommentSerializer < ActiveModel::Serializer
     if scope && scope[:user_id]
       user = User.find(scope[:user_id])
       flag =  true if object.user_id == user.id
+      flag =  true if !object.post.blank? && object.post.user_id == user.id
      # role = object.commentable.user_role(user) if user
      # access = object.commentable.access(role) if role
      # flag =  true if access && access.include?('edit') 
